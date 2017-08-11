f443dd4f39403878c22b7fd0069c85eed9658b9c
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-08-11 17:36:26 +0300

    Edit file pastiePage

--- po/pastiePage.js
+++ po/pastiePage.js
@@ -10,8 +10,22 @@ class PastiePage extends BasePage{
 		this.copyButton=element(by.buttonText('Copy'));
 		this.contentBox=element(by.css('div.images'));
 		this.descText=element(by.css('input.description'));
-
 	}
-	
+
+	// getLinkField(){
+	// 	return this.linkField;
+	// }
+
+	// getCopyButton(){
+	// 	return this.copyButton;
+	// }
+
+	// getContentBox(){
+	// 	return this.contentBox;
+	// }
+
+	// getDescText(){
+	// 	return this.descText;
+	// }
 }
 module.exports=PastiePage;
