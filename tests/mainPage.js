56c702c09ec06774dbcf0fc5d133f27acca61a31
Eugen Stotik <eugen_stotik@epam.com>
2017-08-11 14:44:49 +0300

    Add Base Page object

--- tests/mainPage.js
+++ tests/mainPage.js
@@ -15,19 +15,37 @@ describe('Main page', () => {
     browser.get('https://pasta.lab.epam.com/');
   });
 
+  afterAll( () => {
+    browser.sleep(3000);
+  });
+
   beforeEach(() => {
     console.log("before each");
   });
 
   it('should open the main page', () => {
-    var icon = browser.$('.fa.fa-clipboard');
-    helper.waitForAndClick(icon, helper.waitForVisible);
+    // var icon = browser.$('.fa.fa-clipboard');
+    // helper.waitForAndClick(icon, helper.waitForVisible);
+    //
+    // browser.sleep(1000);
+    // expect(browser.getCurrentUrl()).toEqual('https://pasta.lab.epam.com/pasties/page/1');
+    var BasePage = require('../po/BasePage');
+    var main = new BasePage();
+    main.visit();
+    expect(browser.getCurrentUrl()).toEqual('https://pasta.lab.epam.com/');
 
+    main.getHeader().clickPasties();
     browser.sleep(1000);
     expect(browser.getCurrentUrl()).toEqual('https://pasta.lab.epam.com/pasties/page/1');
+
+    main.getHeader().clickLogo();
+    browser.sleep(1000);
+    expect(browser.getCurrentUrl()).toEqual('https://pasta.lab.epam.com/');
+
+
   });
 
-  it('should click on the header', () => {
+  xit('should click on the header', () => {
     var header = require('../po/ui/Header.js');
     // var header = new Header();
     header.clickLogo();
