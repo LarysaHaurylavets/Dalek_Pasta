9a294ee99bb5598ec2668d1435a0a9cb20550670
Karyna Filatava <karinafilatowa@yandex.ru>
2017-08-15 11:57:38 +0300

    Finished exp date tests

--- tests/karynaTests.js
+++ tests/karynaTests.js
@@ -2,8 +2,6 @@
 var moment = require('moment');
 var url = require('url');
 
-var EC = protractor.ExpectedConditions;
-
 var HomePage = require('../po/HomePage.js');
 var main = new HomePage();
 
@@ -13,11 +11,12 @@ var pastiesPage = new PastiesPage();
 const PastiePage = require('../po/pastiePage');
 var pastiePage = new PastiePage();
 
+
 describe('expiration date', () => {
 
     var ID,
-        actualExpDate,
-        expectedExpDate;
+        expectedExpDate,
+        nowDate;
 
     beforeAll(() => {
         browser.waitForAngularEnabled(false);
@@ -36,29 +35,118 @@ describe('expiration date', () => {
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
             .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(10, 'm').format('DD-MM-YYYY h:mm');
-                main.header.clickPasties();
-                browser.sleep(3000);
-                return main.getUrl();
+                browser.sleep(1000);
+                return pastiePage.getUrl();
             }).then((urlString) => {
                 ID = url.parse(urlString).pathname.split("/").pop();
-                browser.sleep(3000);
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 10min = ${expectedExpDate}, but recieved expiration is ${act}`);
             });
+    });
 
-    //     main.getUrl().then((urlString) => {
-    //         ID = url.parse(urlString).pathname.split("/").pop();
-    //         pastiePage.header.clickPasties();
-    //         return browser.sleep(3000);
-    //    }).then(() => pastiesPage.getExp(ID))
-    //    .then((act) => {
-    //        expect(act).toContain(expectedExpDate);
-    //    });
-    // main.header.clickPasties();
-    // browser.sleep(3000);
-    // pastiesPage.getExp('2emzt7ki').then((text) => expect(text).toContain('15-08-2017 11:23'));
+    it('should display expiration date for 1d', () => {
+        main.setExpirationDate('1d')
+            .then(() => main.setDescription('new'))
+            .then(() => main.uploadUI('txt'))
+            .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
+                expectedExpDate = moment().add(1, 'd').format('DD-MM-YYYY h:mm');
+                browser.sleep(1000);
+                return pastiePage.getUrl();
+            }).then((urlString) => {
+                ID = url.parse(urlString).pathname.split("/").pop();
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
+                return pastiesPage.getExp(ID);
+            }).then((act) => {
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1d = ${expectedExpDate}, but recieved expiration is ${act}`);
+            });
+    });
 
+    it('should display expiration date for 3d', () => {
+        main.setExpirationDate('3d')
+            .then(() => main.setDescription('new'))
+            .then(() => main.uploadUI('txt'))
+            .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
+                expectedExpDate = moment().add(3, 'd').format('DD-MM-YYYY h:mm');
+                browser.sleep(1000);
+                return pastiePage.getUrl();
+            }).then((urlString) => {
+                ID = url.parse(urlString).pathname.split("/").pop();
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
+                return pastiesPage.getExp(ID);
+            }).then((act) => {
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 3d = ${expectedExpDate}, but recieved expiration is ${act}`);
+            });
+    });
+
+    it('should display expiration date for 7d', () => {
+        main.setExpirationDate('7d')
+            .then(() => main.setDescription('new'))
+            .then(() => main.uploadUI('txt'))
+            .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
+                expectedExpDate = moment().add(7, 'd').format('DD-MM-YYYY h:mm');
+                browser.sleep(1000);
+                return pastiePage.getUrl();
+            }).then((urlString) => {
+                ID = url.parse(urlString).pathname.split("/").pop();
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
+                return pastiesPage.getExp(ID);
+            }).then((act) => {
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 7d = ${expectedExpDate}, but recieved expiration is ${act}`);
+            });
+    });
+
+    it('should display expiration date for 2w', () => {
+        main.setExpirationDate('2w')
+            .then(() => main.setDescription('new'))
+            .then(() => main.uploadUI('txt'))
+            .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
+                expectedExpDate = moment().add(2, 'w').format('DD-MM-YYYY h:mm');
+                browser.sleep(1000);
+                return pastiePage.getUrl();
+            }).then((urlString) => {
+                ID = url.parse(urlString).pathname.split("/").pop();
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
+                return pastiesPage.getExp(ID);
+            }).then((act) => {
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 2w = ${expectedExpDate}, but recieved expiration is ${act}`);
+            });
+    });
+
+    it('should display expiration date for 1month', () => {
+        main.setExpirationDate('1mnth')
+            .then(() => main.setDescription('new'))
+            .then(() => main.uploadUI('txt'))
+            .then(() => {
+                nowDate = moment().format('DD-MM-YYYY h:mm');
+                expectedExpDate = moment().add(1, 'M').format('DD-MM-YYYY h:mm');
+                browser.sleep(1000);
+                return pastiePage.getUrl();
+            }).then((urlString) => {
+                ID = url.parse(urlString).pathname.split("/").pop();
+                return pastiePage.header.clickPasties();
+            }).then(() => {
+                browser.sleep(1000);
+                return pastiesPage.getExp(ID);
+            }).then((act) => {
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1month = ${expectedExpDate}, but recieved expiration is ${act}`);
+            });
     });
 });
