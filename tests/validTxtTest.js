2e24b1458cdc1c50e7ebcc876e47e829634032d1
Karyna <karinafilatowa@yandex.ru>
2017-08-15 09:41:21 +0300

    Update validTxtTest.js

--- tests/validTxtTest.js
+++ tests/validTxtTest.js
@@ -21,20 +21,6 @@ xdescribe('upload file on main page', () => {
         browser.sleep(1000);
     });  
 
-<<<<<<< HEAD
-
-
-    it('should open main page', function(){
-        main.visit();
-        expect(browser.getCurrentUrl()).toEqual('https://pasta.lab.epam.com/');
-
-    });
-
-
-    it('should upload valid txt file', function(){
-        main.uploadUI('txt');
-    });
-=======
     it('should upload valid txt file', function(){   
         main.setExpirationDate('10min');
         main.uploadUI('txt');       
@@ -51,7 +37,7 @@ xdescribe('upload file on main page', () => {
         browser.wait(EC.elementToBeClickable(pastiePage.copyButton), 5000).then(()=>{
             expect(pastiePage.copyButton.isPresent()).toBe(true);
         });  
->>>>>>> 88b52c4a941100e863fc6a439853f853b112a5d8
+
 
         browser.wait(EC.elementToBeClickable(pastiePage.syntaxLabel), 5000).then(()=>{
             expect(pastiePage.syntaxLabel.isPresent()).toBe(true);  
