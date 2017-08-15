207dd426b0c7895b2e840f21a008c23f6cfc13ca
Karyna Filatava <karinafilatowa@yandex.ru>
2017-08-15 12:14:17 +0300

    Test

--- tests/karynaTests.js
+++ tests/karynaTests.js
@@ -50,11 +50,11 @@ describe('expiration date', () => {
             });
     });
 
-    xit('should display expiration date for 1d', () => {
+    it('should display expiration date for 1d', () => {
         main.setExpirationDate('1d')
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
-            .then(() => main.uploadGo())
+            .then(() => main.shareGo())
             .then(() => {
                 nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(1, 'd').format('DD-MM-YYYY h:mm');
@@ -67,15 +67,15 @@ describe('expiration date', () => {
                 browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1d = ${expectedExpDate}, but recieved expiration is ${act}`);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1d = ${expectedExpDate}, but recieved expiration for pastie with ID ${ID} is ${act}`);
             });
     });
 
-    xit('should display expiration date for 3d', () => {
+    it('should display expiration date for 3d', () => {
         main.setExpirationDate('3d')
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
-            .then(() => main.uploadGo())
+            .then(() => main.shareGo())
             .then(() => {
                 nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(3, 'd').format('DD-MM-YYYY h:mm');
@@ -88,15 +88,15 @@ describe('expiration date', () => {
                 browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 3d = ${expectedExpDate}, but recieved expiration is ${act}`);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 3d = ${expectedExpDate}, but recieved expiration for pastie with ID ${ID} is ${act}`);
             });
     });
 
-    xit('should display expiration date for 7d', () => {
+    it('should display expiration date for 7d', () => {
         main.setExpirationDate('7d')
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
-            .then(() => main.uploadGo())
+            .then(() => main.shareGo())
             .then(() => {
                 nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(7, 'd').format('DD-MM-YYYY h:mm');
@@ -109,15 +109,15 @@ describe('expiration date', () => {
                 browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 7d = ${expectedExpDate}, but recieved expiration is ${act}`);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 7d = ${expectedExpDate}, but recieved expiration for pastie with ID ${ID} is ${act}`);
             });
     });
 
-    xit('should display expiration date for 2w', () => {
+    it('should display expiration date for 2w', () => {
         main.setExpirationDate('2w')
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
-            .then(() => main.uploadGo())
+            .then(() => main.shareGo())
             .then(() => {
                 nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(2, 'w').format('DD-MM-YYYY h:mm');
@@ -130,15 +130,15 @@ describe('expiration date', () => {
                 browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 2w = ${expectedExpDate}, but recieved expiration is ${act}`);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 2w = ${expectedExpDate}, but recieved expiration for pastie with ID ${ID} is ${act}`);
             });
     });
 
-    xit('should display expiration date for 1month', () => {
+    it('should display expiration date for 1month', () => {
         main.setExpirationDate('1mnth')
             .then(() => main.setDescription('new'))
             .then(() => main.uploadUI('txt'))
-            .then(() => main.uploadGo())
+            .then(() => main.shareGo())
             .then(() => {
                 nowDate = moment().format('DD-MM-YYYY h:mm');
                 expectedExpDate = moment().add(1, 'M').format('DD-MM-YYYY h:mm');
@@ -151,7 +151,7 @@ describe('expiration date', () => {
                 browser.sleep(1000);
                 return pastiesPage.getExp(ID);
             }).then((act) => {
-                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1month = ${expectedExpDate}, but recieved expiration is ${act}`);
+                expect(act).toContain(expectedExpDate,`test execution date ${nowDate} + 1month = ${expectedExpDate}, but recieved expiration for pastie with ID ${ID} is ${act}`);
             });
     });
 });
