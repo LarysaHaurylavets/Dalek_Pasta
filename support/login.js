8a451fd695ace77c563f1abd92cc6f476e18d517
Karyna Filatava <karinafilatowa@yandex.ru>
2017-08-21 11:35:17 +0300

    And final-final changes

--- support/login.js
+++ support/login.js
@@ -12,10 +12,11 @@ describe('login', () => {
     it('should show an alert with defined text for invalid-size img', () => {
 
         browser.driver.switchTo().activeElement();
-        browser.sleep(2000);
+        browser.sleep(5000);
        exec('./support/login.exe', function(err, data) {
            console.log(err);
            console.log(data.toString());
        });
+       browser.sleep(30000);
     });
 });
